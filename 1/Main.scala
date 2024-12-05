7934b66cc4a91513ab8b263006aca141520d7a82
daromi94 <davromero07@gmail.com>
2024-12-04 20:47:13 -0500

    refactor: :recycle: handle number format errors in day 1

--- 1/Main.scala
+++ 1/Main.scala
@@ -5,17 +5,20 @@ type Locations    = Seq[Location]
 type LocationPair = (Location, Location)
 
 def readLocations(lines: Iterator[String]): (Locations, Locations) =
-  lines.map { parseLine }.toSeq.unzip
+  lines.zipWithIndex.map { parseLine }.toSeq.unzip
 
-def parseLine(line: String): LocationPair =
-  line.split("\\s+").toList match
-    case l :: r :: Nil => (l.toInt, r.toInt)
-    case _             => throw new IllegalArgumentException(s"invalid line format: '$line'")
+def parseLine(line: String, index: Int): LocationPair =
+  try
+    line.split("\\s+").toList match
+      case l :: r :: Nil => (l.toInt, r.toInt)
+      case _             => throw new IllegalArgumentException(s"line ${index + 1}: expected two elements: '$line'")
+  catch
+    case _: NumberFormatException => throw new IllegalArgumentException(s"line ${index + 1}: invalid number: '$line'")
 
 def calculateTotalDistance(left: Locations, right: Locations): Int =
   val (leftSorted, rightSorted) = (left.sorted, right.sorted)
 
-  val distances = leftSorted.zip(rightSorted).map { (l, r) => Math.abs(r - l) }
+  val distances = leftSorted.zip(rightSorted).map { (l, r) => math.abs(r - l) }
 
   distances.sum
 
