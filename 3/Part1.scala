ceb396dc2f4dfd49e95bddf340f45c4ee21cbe70
daromi94 <davromero07@gmail.com>
2024-12-09 20:47:21 -0500

    refactor: :recycle: join sections in day 3, part 1

--- 3/Part1.scala
+++ 3/Part1.scala
@@ -1,27 +1,29 @@
 import scala.io.Source
 import scala.util.matching.Regex
 
-object Scanner:
+object Interpreter:
+
   private val Instruction: Regex = """mul\((\d+),(\d+)\)""".r
 
-  def scan(sections: Iterator[String]): Int =
-    sections.map { scan }.sum
+  def interpret(sections: Iterator[String]): Int =
+    interpret(sections.mkString)
 
-  def scan(section: String): Int =
+  private def interpret(section: String): Int =
     val instructions = Instruction.findAllIn(section)
 
-    instructions.collect {
+    val products = instructions.collect {
       case Instruction(a, b) =>
         try
           a.toInt * b.toInt
         catch
           case e: NumberFormatException => throw new RuntimeException(s"$a * $b: invalid instruction", e)
-    }.sum
+    }
+
+    products.sum
 
 @main def main(args: String*): Unit =
-  val lines = Source.stdin.getLines
+  val lines = Source.stdin.getLines()
 
-  // Part 1
-  val res = Scanner.scan(lines)
+  val res = Interpreter.interpret(lines)
 
   println(s"Result: $res")
